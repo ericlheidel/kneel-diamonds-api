e583d7a70f1ecb16dd17c47b955054a98c4e12ad
Eric Heidel <ericleeheidel@yahoo.com>
2024-02-27 15:48:17 -0600

    End Chapter 4

--- views/order_view.py
+++ views/order_view.py
@@ -30,3 +30,29 @@ def list_orders():
         serialized_orders = json.dumps(orders)
 
     return serialized_orders
+
+
+def get_single_order(pk):
+    with sqlite3.connect("./kneeldiamonds.sqlite3") as connection:
+        connection.row_factory = sqlite3.Row
+        database_cursor = connection.cursor()
+
+        database_cursor.execute(
+            """
+                SELECT
+                    o.id,
+                    o.metal_id,
+                    o.size_id,
+                    o.style_id,
+                    o.type_id
+                FROM Orders o
+                WHERE o.id = ?
+            """,
+            (pk,),
+        )
+        query_results = database_cursor.fetchone()
+
+        dictionary_version_of_object = dict(query_results)
+        serialized_order = json.dumps(dictionary_version_of_object)
+
+        return serialized_order
