2a6c8ea2b68c54ce0adc75f850b012322cb40e21
Eric Heidel <ericleeheidel@yahoo.com>
2024-02-28 16:19:15 -0600

    Lesson Complete

--- views/__init__.py
+++ views/__init__.py
@@ -1,1 +1,2 @@
-from .order_view import list_orders, get_single_order
+from .order_view import list_orders, get_single_order, post_order, delete_order
+from .metal_view import list_metals, get_single_metal, update_metal
